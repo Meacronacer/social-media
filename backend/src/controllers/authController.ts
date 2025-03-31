9b93139215050c2d595dcb876ef91e24c6d7803c
Ivan <itanmare@gmail.com>
2025-03-31 11:20:05 +0300

    backend

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -5,7 +5,7 @@ import tokenService from "../services/token.service";
 import UserDto from "../dtos/user-dto";
 import { validationResult } from "express-validator";
 import { IUser } from "../models/User";
-import { config } from "../config/env";
+//import { config } from "../config/env";
 
 class AuthController {
   async registration(
@@ -45,19 +45,11 @@ class AuthController {
         httpOnly: true,
         secure: true,
         sameSite: "none",
-        path: "/",
-        domain: config.isProduction
-          ? "social-media-production-b56c.up.railway.app"
-          : undefined,
       });
       res.cookie("refreshToken", userData.refreshToken, {
         httpOnly: true,
         secure: true,
         sameSite: "none",
-        path: "/",
-        domain: config.isProduction
-          ? "social-media-production-b56c.up.railway.app"
-          : undefined,
       });
       res.status(200).json({ message: "you logged in" });
     } catch (e) {
@@ -82,19 +74,11 @@ class AuthController {
           httpOnly: true,
           secure: true,
           sameSite: "none",
-          path: "/",
-          domain: config.isProduction
-            ? "social-media-production-b56c.up.railway.app"
-            : undefined,
         });
         res.cookie("refreshToken", refreshToken, {
           httpOnly: true,
           secure: true,
           sameSite: "none",
-          path: "/",
-          domain: config.isProduction
-            ? "social-media-production-b56c.up.railway.app"
-            : undefined,
         });
 
         res.redirect(process.env.CLIENT_URL as string);
@@ -118,10 +102,6 @@ class AuthController {
           httpOnly: true,
           secure: true,
           sameSite: "none",
-          path: "/",
-          domain: config.isProduction
-            ? "social-media-production-b56c.up.railway.app"
-            : undefined,
         });
         res.json({ message: "Access token refreshed!" });
       }
